ceb94c790e23129ec3eb9f0e30f01aed410eef2c
ali32b <shafyee@gmail.com>
2024-10-15 18:07:15 +0330

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,7 +6,7 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = 'd945412d-1cab-4a42-996c-60b4c94fa635';
+let userID = 'd945412d-1cab-4a42-996c-60b4c94fa635' ;
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
